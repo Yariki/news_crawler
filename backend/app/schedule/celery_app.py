ca80b45de0f312fd455c175432c4012094a73590
Yaroslav yariki Lisovskyi <iyariki.ya@gmail.com>
2026-06-16 01:11:28 +0300

    Small refactorings and settings changes

--- backend/app/schedule/celery_app.py
+++ backend/app/schedule/celery_app.py
@@ -1,9 +1,14 @@
 from celery import Celery
 from ..core.config import settings
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG if settings.app_mode == "dev" else logging.INFO,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 celery_app = Celery(
     "celery_app",
-      broker=settings.celery_broker_url, 
+      broker=settings.celery_broker_url,
       include=["app.schedule.tasks.dispatch_sources", "app.schedule.tasks.check_source"]
 )
 
@@ -13,3 +18,5 @@ celery_app.conf.worker_prefetch_multiplier = 1
 celery_app.conf.task_routes = {
     "schedule.tasks.run_scheduled_job": {"queue": settings.celery_task_queue},
 }
+
+logging.info("Celery app configured and ready to run...")
